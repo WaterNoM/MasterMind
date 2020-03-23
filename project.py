8bb982f6d0df0c6cf27cc1537f7cff40ae822019
WaterNoM <62561317+WaterNoM@users.noreply.github.com>
2020-03-23 11:17:20 -0700

    Update project.py

--- project.py
+++ project.py
@@ -9,34 +9,46 @@ def create_password():
     password.append(2)
     password.append(3)
     password.append(4)
-    
+
 def get_guess():
     temp = []
-    for i in range(4):# [0, 1, 2, 3, 4]
+    for i in range(4): # [0, 1, 2, 3]
         word = input("Give me a number fool!") # "12"
         num = int(word)
         temp.append(num)
     return temp
-    
+
 def is_correct(guess):
+    global close
+    
+    flag = True
+    close = 0
+    
     for i in range(4):
         if guess[i] != password[i]:
-            return False
-    
-    return True
-    
+            flag = False
+            
+            if guess[i] in password:
+                close += 1
+            
+            print("_")
+            
+        else:
+            print(guess[i])
+            
+    return flag
+
 def main():
     # Create the password first
     create_password()
     
     guess = get_guess()
-    while is_correct(guesss) == False:
+    while is_correct(guess) == False:
         print("You got it wrong buster!")
-        guess = get_guess
-        
-    print("Correct!!!")
+        guess = get_guess()
     
+    print("Correct!!!!")
 
-    
 main()
-    
+
+
